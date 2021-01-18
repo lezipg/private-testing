ef3619e46edb6797004e433c23ba9e94a0699385
lez-ka <58781880+lezipg@users.noreply.github.com>
2021-01-18 09:52:47 +0100

    add issueDouble

--- main.cc
+++ main.cc
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <iostream>
+
+int issueDouble()
+{
+    double x = 1;
+    int n = 10;
+    while (n--)
+    {
+        int a = (x + 0.001 * n) * 1000;
+        std::cout << a << std::endl;
+    }
+}
+
 int main()
 {
-    std::cout<<"hello world!"<<std::endl;
-    std::cout<<"new branch"<<std::endl;
+    std::cout << "hello world!" << std::endl;
+    std::cout << "new branch" << std::endl;
     return 0;
 }
